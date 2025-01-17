aeca9901bfc2294072838d639a312e2b098389a1
Hovo Petrosyan <hovpetrosyan.github@gmail.com>
2025-01-17 21:20:27 +0400

    feat(component): FormInput is created for easy management

---
+++ src/components/FormInput/index.tsx
@@ -0,0 +1,18 @@
+export default function FormInput({
+    field,
+    labelName
+}) {
+    return (<>
+        <label htmlFor={field.name} className="block text-sm font-medium text-gray-700">{labelName}: </label>
+        <input
+            name={field.name}
+            value={field.state.value}
+            onBlur={field.handleBlur}
+            onChange={(e) => field.handleChange(e.target.value)}
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+        />
+        {field.state.meta.errors ? (
+            <p role="alert" className="mt-1 text-sm text-red-600">{field.state.meta.errors.join(', ')}</p>
+        ) : null}
+    </>)
+};
